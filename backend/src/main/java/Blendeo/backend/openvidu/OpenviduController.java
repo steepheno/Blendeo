07974710f717790080b06ce91ab56911b8bff14b
Dahee Park <ekgml3219@naver.com>
2025-01-22 17:40:22 +0900

    :art: [FIX] 환경변수 이름 변경

---
+++ backend/src/main/java/Blendeo/backend/openvidu/OpenviduController.java
@@ -0,0 +1,60 @@
+package Blendeo.backend.openvidu;
+
+
+
+import io.openvidu.java.client.*;
+
+import jakarta.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+
+@CrossOrigin(origins="http://localhost:3000")
+@RequestMapping("/api/v1/sessions")
+@RestController
+public class OpenviduController {
+
+    @Value("${openvidu.url}")
+    private String OPENVIDU_URL;
+
+    @Value("${openvidu.secret}")
+    private String OPENVIDU_SECRET;
+
+    private OpenVidu openvidu;
+
+    @PostConstruct
+    public void init(){
+        this.openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+    }
+
+    /* session properties, return Seesion ID */
+    @PostMapping
+    public ResponseEntity<String> initializeSession(@RequestBody(required = false) Map<String, Object> params)
+            throws OpenViduJavaClientException, OpenViduHttpException {
+        SessionProperties properties = SessionProperties.fromJson(params).build();
+        Session session = openvidu.createSession(properties);
+        return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
+
+    }
+
+    @PostMapping("/{session_id}/connections")
+    public ResponseEntity<String> createConnection(@PathVariable("session_id") String sessionId,
+                                                   @RequestBody(required = false) Map<String, Object> params)
+            throws OpenViduJavaClientException, OpenViduHttpException {
+        Session session = openvidu.getActiveSession(sessionId);
+        if (session == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
+        Connection connection = session.createConnection(properties);
+        return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
+    }
+
+
+
+}
