d288717340cbca32e4cf5841ce13e586cc1e91a9
김영진 <kyjjulia07@gmail.com>
2025-01-28 21:13:49 +0900

    :sparkles: [FEAT] 메일 인증 CORS 추가

--- backend/src/main/java/Blendeo/backend/config/WebConfig.java
+++ backend/src/main/java/Blendeo/backend/config/WebConfig.java
@@ -1,15 +1,26 @@
 package Blendeo.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${server.address}")
+    String originUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:5173",
+                        "http://i12a602.p.ssafy.io:3000",
+                        "http://i12a602.p.ssafy.io:5173"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false)
