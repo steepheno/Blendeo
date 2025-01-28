a01e53855293f22b40f40c56ed317f77f113e1c1
김민서 <ms9648@naver.com>
2025-01-28 16:32:12 +0900

    :poop [FIX] 세미콜론 누락

--- backend/src/main/java/Blendeo/backend/global/error/ErrorCode.java
+++ backend/src/main/java/Blendeo/backend/global/error/ErrorCode.java
@@ -20,7 +20,7 @@ public enum ErrorCode {
     // Comment
     INVALID_COMMENT_CONTENT(HttpStatus.BAD_REQUEST, "댓글 내용은 필수입니다."),
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글이 존재하지 않습니다."),
-    COMMENT_TOO_LONG(HttpStatus.BAD_REQUEST, "댓글이 최대 길이를 초과했습니다."),
+    COMMENT_TOO_LONG(HttpStatus.BAD_REQUEST, "댓글이 최대 길이를 초과했습니다.");
 
     private final HttpStatus status;
     private final String message;
