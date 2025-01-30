78f4e7489d350cf2134929c6dc4bf456040c8ff1
김민서 <ms9648@naver.com>
2025-01-30 14:33:58 +0900

    :art: [FIX] ResponseEntity로 응답 개선

--- backend/src/main/java/Blendeo/backend/project/controller/RankingController.java
+++ backend/src/main/java/Blendeo/backend/project/controller/RankingController.java
@@ -4,6 +4,7 @@ import Blendeo.backend.project.dto.ProjectRankRes;
 import Blendeo.backend.project.service.RankingService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class RankingController {
     private final RankingService rankingService;
 
     @GetMapping("/likes")
-    public List<ProjectRankRes> getRankingByLikes(){
-        return rankingService.getRankingByLikes();
+    public ResponseEntity<List<ProjectRankRes>> getRankingByLikes(){
+        return ResponseEntity.ok(rankingService.getRankingByLikes());
     }
 }
