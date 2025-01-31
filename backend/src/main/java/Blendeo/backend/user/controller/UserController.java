d24b30ba315b05e13b1ab298b41029144b0ea89a
김영진 <kyjjulia07@gmail.com>
2025-01-31 12:46:12 +0900

    :sparkles: [FEAT] STMP CORS 5173 해결

--- backend/src/main/java/Blendeo/backend/user/controller/UserController.java
+++ backend/src/main/java/Blendeo/backend/user/controller/UserController.java
@@ -16,17 +16,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 @RequestMapping("/api/v1/user")
 @RestController
 public class UserController {
