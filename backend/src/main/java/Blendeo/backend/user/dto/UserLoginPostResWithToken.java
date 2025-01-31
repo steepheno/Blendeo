ea34f458ee08699f3916530629f94bffbe1fc9a4
김영진 <kyjjulia07@gmail.com>
2025-01-31 16:08:11 +0900

    :sparkles: [FEAT] 로그인 token cookie에 넣기

--- backend/src/main/java/Blendeo/backend/user/dto/UserLoginPostResWithToken.java
+++ backend/src/main/java/Blendeo/backend/user/dto/UserLoginPostResWithToken.java
@@ -1,15 +1,15 @@
 package Blendeo.backend.user.dto;
 
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 @Builder
-public class UserLoginPostRes {
+public class UserLoginPostResWithToken {
     private int id;
     private String email;
     private String nickname;
     private String profileImage;
+    private String accessToken;
+    private String refreshToken;
 }
