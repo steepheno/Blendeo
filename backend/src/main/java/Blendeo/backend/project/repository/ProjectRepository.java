910e7bb760d590a616e11e73381fe4a1cf723749
Dahee Park <ekgml3219@naver.com>
2025-02-06 13:55:03 +0900

    :sparkles: [FEAT] add find author query

--- backend/src/main/java/Blendeo/backend/project/repository/ProjectRepository.java
+++ backend/src/main/java/Blendeo/backend/project/repository/ProjectRepository.java
@@ -1,7 +1,13 @@
 package Blendeo.backend.project.repository;
 
 import Blendeo.backend.project.entity.Project;
+import Blendeo.backend.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    @Query("select p.author from Project p where p.id= :id")
+    Optional<User> findAuthorById(Long id);
 }
