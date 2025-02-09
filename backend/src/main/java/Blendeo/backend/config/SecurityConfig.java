fced6f65efb85570fccf7c8ef12db2b9f252cd9f
김영진 <kyjjulia07@gmail.com>
2025-02-09 20:14:57 +0900

    :sparkles: [FEAT] ã httpBasic 활성화 -> 비활성화

--- backend/src/main/java/Blendeo/backend/config/SecurityConfig.java
+++ backend/src/main/java/Blendeo/backend/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -71,7 +72,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class) // JWT 필터 추가
-                .httpBasic(Customizer.withDefaults()); // HTTP Basic 인증 활성화
+                .httpBasic(AbstractHttpConfigurer::disable);// HTTP Basic 인증 비활성화
         return http.build();
     }
 }
