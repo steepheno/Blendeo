63da20a16bb4fb17d0106f29175e5baf7e4706cb
김영진 <kyjjulia07@gmail.com>
2025-02-10 15:02:35 +0900

    :art: [CHORE] swagger  CORS 추가

--- backend/src/main/java/Blendeo/backend/config/WebConfig.java
+++ backend/src/main/java/Blendeo/backend/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:5173",
                         "http://i12a602.p.ssafy.io:5173",
