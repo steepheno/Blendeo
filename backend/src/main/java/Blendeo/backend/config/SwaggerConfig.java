a7edcdf59633bcd026edd7e730d183b803f62d03
김영진 <kyjjulia07@gmail.com>
2025-02-10 21:09:20 +0900

    :art: [FIX] Swagger CORS 권í오류로 도메일 일치시키기 추기

--- backend/src/main/java/Blendeo/backend/config/SwaggerConfig.java
+++ backend/src/main/java/Blendeo/backend/config/SwaggerConfig.java
@@ -28,8 +28,11 @@ public class SwaggerConfig {
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
 
         return new OpenAPI().servers(List.of(
-                        new Server().url("https://api.blendeo.shop")  // HTTPS URL로 설정
-                        ))
+                        new Server().url("https://api.blendeo.shop"),  // HTTPS URL로 설정
+                        new Server().url("http://api.blendeo.shop"),
+                        new Server().url("http://localhost:8080"),
+                        new Server().url("http://http://i12a602.p.ssafy.io:8080/")
+                ))
                         .components(new Components()
                         .addSecuritySchemes("bearerAuth", securityScheme)) // API 구성 요소를 설정합니다.
                 .security(Arrays.asList(securityRequirement))
