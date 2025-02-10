85086f3bf9afa09da0e30f39f51815e58841f9d3
김영진 <kyjjulia07@gmail.com>
2025-02-10 16:35:53 +0900

    :art: [CHORE] swagger CORS 설정]

--- backend/src/main/java/Blendeo/backend/BackendApplication.java
+++ backend/src/main/java/Blendeo/backend/BackendApplication.java
@@ -1,11 +1,14 @@
 package Blendeo.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "https://api.blendeo.shop", description = "Default Server URL")})
 public class BackendApplication {
 
     public static void main(String[] args) {
