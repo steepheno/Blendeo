6737a45a87ad8ebfdc5e8b629e5e3436520431f9
김영진 <kyjjulia07@gmail.com>
2025-02-10 21:11:42 +0900

    :art: [FIX] ãCORS 배포 환경 설정

--- backend/src/main/java/Blendeo/backend/config/SecurityConfig.java
+++ backend/src/main/java/Blendeo/backend/config/SecurityConfig.java
@@ -41,9 +41,7 @@ public class SecurityConfig {
                 "https://blendeo.shop",
                 "http://blendeo.shop",           // HTTP 버전 추가
                 "https://api.blendeo.shop",      // API 도메인 추가
-                "http://api.blendeo.shop",        // API 도메인 HTTP 버전 추가
-                "http://api.blendeo.shop/swagger-ui/index.html",
-                "https://api.blendeo.shop/swagger-ui/index.html"
+                "http://api.blendeo.shop"        // API 도메인 HTTP 버전 추가
                 ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
