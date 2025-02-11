3ea7deb3f03ad60158aa3fb5c9ad703c00b7ccb4
김민서 <ms9648@naver.com>
2025-02-11 14:56:54 +0900

    :sparkles: [FEAT] 랭킹/조회수 기반 프로젝트 조회 페이징 처리 구현

--- backend/src/main/java/Blendeo/backend/project/controller/RankingController.java
+++ backend/src/main/java/Blendeo/backend/project/controller/RankingController.java
@@ -1,5 +1,7 @@
 package Blendeo.backend.project.controller;
 
+import Blendeo.backend.project.dto.ProjectInfoRes;
+import Blendeo.backend.project.dto.ProjectListDto;
 import Blendeo.backend.project.dto.ProjectRankRes;
 import Blendeo.backend.project.service.RankingService;
 import java.util.List;
@@ -7,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,12 +20,12 @@ public class RankingController {
     private final RankingService rankingService;
 
     @GetMapping("/likes")
-    public ResponseEntity<List<ProjectRankRes>> getRankingByLikes(){
-        return ResponseEntity.ok(rankingService.getRankingByLikes());
+    public ResponseEntity<List<ProjectListDto>> getRankingByLikes(@RequestParam("size") int size, @RequestParam("page") int page){
+        return ResponseEntity.ok(rankingService.getRankingByLikes(size, page));
     }
 
     @GetMapping("/views")
-    public ResponseEntity<List<ProjectRankRes>> getRankingByViews(){
-        return ResponseEntity.ok(rankingService.getRankingByViews());
+    public ResponseEntity<List<ProjectListDto>> getRankingByViews(@RequestParam("size") int size, @RequestParam("page") int page){
+        return ResponseEntity.ok(rankingService.getRankingByViews(size, page));
     }
 }
