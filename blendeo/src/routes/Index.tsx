6dc2103310b43c73848d4f1ef1b0d777e3ec5a75
kimhyebin <bin5459@naer.com>
2025-02-12 03:25:24 +0900

    :art: [CHORE] 라우터 정리, 프로젝트 트리 경로 수정

--- blendeo/src/routes/Index.tsx
+++ blendeo/src/routes/Index.tsx
@@ -8,18 +8,12 @@ import MainPage from "@/pages/main/MainPage";
 import ChatPage from "@/pages/chat/ChatPage";
 
 import ProjectDetailPage from "@/pages/project/ProjectDetailPage";
-// import SeedRecordPage from "@/pages/seed/SeedRecordPage";
-// import SeedEditPage from "@/pages/seed/SeedEditPage";
-// import SeedUploadPage from "@/pages/seed/SeedUploadPage";
 import ForkRecordPage from "@/pages/project/ForkRecordPage";
-// import ForkEditPage from "@/pages/project/ForkEditPage";
-// import ForkUploadPage from "@/pages/project/ForkUploadPage";
 
 import ProjectTreePage from "@/pages/project/ProjectTreePage";
 import UserProfilePage from "@/pages/profile/UserProfilePage";
 import MyProfilePage from "@/pages/profile/MyProfilePage";
 import NotFoundPage from "@/pages/error/NotFoundPage";
-// import VideoCallPage from "@/pages/chat/VideoCallPage";
 
 export const router = createBrowserRouter([
   {
@@ -40,34 +34,6 @@ export const router = createBrowserRouter([
         path: ":projectId",
         element: <ProjectDetailPage />,
       },
-
-      // 최초 촬영 라우팅
-      // {
-      //   path: "record",
-      //   element: (
-      //     <ProtectedRoute>
-      //       <SeedRecordPage />
-      //     </ProtectedRoute>
-      //   ),
-      // },
-      // {
-      //   path: "edit",
-      //   element: (
-      //     <ProtectedRoute>
-      //       <SeedEditPage />
-      //     </ProtectedRoute>
-      //   ),
-      // },
-      // {
-      //   path: "upload",
-      //   element: (
-      //     <ProtectedRoute>
-      //       <SeedUploadPage />
-      //     </ProtectedRoute>
-      //   ),
-      // },
-
-      // 포크 관련 라우팅
       {
         path: "forkrecord",
         element: (
@@ -76,24 +42,8 @@ export const router = createBrowserRouter([
           </ProtectedRoute>
         ),
       },
-      // {
-      //   path: "forkedit",
-      //   element: (
-      //     <ProtectedRoute>
-      //       <ForkEditPage />
-      //     </ProtectedRoute>
-      //   ),
-      // },
-      // {
-      //   path: "forkupload",
-      //   element: (
-      //     <ProtectedRoute>
-      //       <ForkUploadPage />
-      //     </ProtectedRoute>
-      //   ),
-      // },
       {
-        path: "tree/:projectId",
+        path: ":projectId/tree",
         element: <ProjectTreePage />,
       },
     ],
@@ -128,14 +78,6 @@ export const router = createBrowserRouter([
           </ProtectedRoute>
         ),
       },
-      // {
-      //   path: "video", // /chat/video
-      //   element: (
-      //     <ProtectedRoute>
-      //       <VideoCallPage />
-      //     </ProtectedRoute>
-      //   ),
-      // },
     ],
   },
   {
