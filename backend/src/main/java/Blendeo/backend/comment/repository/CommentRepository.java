e5c7dc98adb1b87f3b7e57a11c67ef7087aa7715
Dahee Park <ekgml3219@naver.com>
2025-02-17 11:10:49 +0900

    :art: [FIX] Repository 어노테이션 추가

--- backend/src/main/java/Blendeo/backend/comment/repository/CommentRepository.java
+++ backend/src/main/java/Blendeo/backend/comment/repository/CommentRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Optional<List<Comment>> findCommentByProjectId(Long projectId);
 
